5565ef19567e2f3bcccd96ed9d87e3a435f80245
joao vitor <joaovitor646260@gmail.com>
2025-08-11 19:59:49 -0300

    update: apenas adicionei uma função no APP

--- Instalando-React/react/src/App.jsx
+++ Instalando-React/react/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 
+function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Button() {
+  return (
+    <div>
+      <button>click</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
+      <Header />
+      <Button />
       <h1>Hello world</h1>
       <h2>Vou ser o melhor fullstack</h2>
     </div>
